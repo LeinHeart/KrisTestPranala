29db77133a03492a8ef5b7031247d7ddbfccb382
LeinHeart <kristopher.chayadi1688@gmail.com>
2023-12-07 11:41:01 +0700

    First Commit. Adding OnboardingViews & OnboardingViews

---
+++ TestPranala/Views/LoginViews.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViews.swift
+//  TestPranala
+//
+//  Created by IT Fagetti on 07/12/23.
+//
+
+import SwiftUI
+
+struct LoginViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginViews_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginViews()
+    }
+}
---
+++ TestPranala/Views/OnboardingViews.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingViews.swift
+//  TestPranala
+//
+//  Created by IT Fagetti on 07/12/23.
+//
+
+import SwiftUI
+
+struct OnboardingViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingViews_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingViews()
+    }
+}
