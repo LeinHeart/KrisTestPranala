c23831b9815aea1d07ebb2c123491851fb2eed58
LeinHeart <kristopher.chayadi1688@gmail.com>
2023-12-07 10:30:22 +0700

    Initial Commit

---
+++ TestPranala/TestPranalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPranalaApp.swift
+//  TestPranala
+//
+//  Created by IT Fagetti on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TestPranalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
